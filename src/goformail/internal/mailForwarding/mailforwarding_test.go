15a65710735d95f80c28b4db834b3d0037ebdf7f
sdk194 <seandags750@gmail.com>
2025-03-28 15:43:57 +0000

    GFM-37/test: Add test for getting current time for logging

---
+++ src/goformail/internal/mailForwarding/mailforwarding_test.go
@@ -0,0 +1,20 @@
+package mailforwarding
+
+import (
+	"log"
+	"regexp"
+	"testing"
+)
+
+func TestGetCurrentTime(t *testing.T) {
+	formattedTime := getCurrentTime()
+
+	// EXPECTED FORMAT [YYYY-MM-DD HH:MM:SS]
+	matched, err := regexp.Match(`\[([0-9]{4})(-([0-9]{2})){2}\s([0-5][0-9]:){2}[0-5][0-9]]`, []byte(formattedTime))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !matched {
+		t.Errorf("Not the expected format of [YYYY-MM-DD HH:mm:ss], got %s", formattedTime)
+	}
+}
