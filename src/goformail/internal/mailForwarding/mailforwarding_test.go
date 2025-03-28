bbb33760713d9e29c5a6554047696ca562afbbf3
sdk194 <seandags750@gmail.com>
2025-03-28 22:01:25 +0000

    GFM-37/test: Add test case of sendGoodbye function

--- src/goformail/internal/mailForwarding/mailforwarding_test.go
+++ src/goformail/internal/mailForwarding/mailforwarding_test.go
@@ -1,12 +1,15 @@
 package mailforwarding
 
 import (
+	"errors"
 	"github.com/joho/godotenv"
 	"log"
 	"net"
 	"regexp"
+	"strings"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestGetCurrentTime(t *testing.T) {
@@ -123,4 +126,78 @@ func TestSendResponse(t *testing.T) {
 	if string(buffer[:size]) != "Response test" {
 		t.Error("There were no responses/response was wrong")
 	}
+}
+
+func TestSendGoodBye(t *testing.T) {
+	waitGroup := new(sync.WaitGroup)
+	go func() {
+		tcpSocket, err := net.Listen("tcp", "127.0.0.1:8025")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		waitGroup.Done()
+		conn, err := tcpSocket.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// POSITIVE CASE
+		sendGoodbye(conn, true, "QUIT")
+
+		// NEGATIVE CASE
+		sendGoodbye(conn, false, "QUIT")
+	}()
+	waitGroup.Add(1)
+	waitGroup.Wait()
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8025")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(conn net.Conn) {
+		err = conn.Close()
+		if err != nil {
+
+		}
+	}(conn)
+
+	buffer := make([]byte, 4096)
+	var size int
+	var collected string
+
+	for i := 0; i < 4; i++ {
+		if err = conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+			log.Fatal(err)
+		}
+
+		size, err = conn.Read(buffer)
+		var netErr net.Error
+		if errors.As(err, &netErr) && netErr.Timeout() {
+			t.Error("Connection timed out, messages were not sent through connection")
+			return
+		}
+
+		collected += string(buffer[:size])
+	}
+
+	messages := strings.Lines(collected)
+	passedTests := 0
+	for message := range messages {
+		switch {
+		case strings.TrimSpace(message) == "250 OK (Email was successfully forwarded)":
+			passedTests += 1
+		case strings.TrimSpace(message) == "452 temporarily over quota":
+			passedTests += 1
+		case strings.TrimSpace(message) == "250 OK (However, email was not forwarded)":
+			passedTests += 1
+		case strings.TrimSpace(message) == "221 closing connection":
+			passedTests += 1
+		}
+	}
+	if passedTests != 6 {
+		t.Errorf("Not all cases were resolved for sending goodbye acknowledgements (only %d passed)", passedTests)
+	}
+
 }
