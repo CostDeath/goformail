75fa39d48a2f09a8c9f1eb2d7228da624067a391
sdk194 <seandags750@gmail.com>
2025-03-28 13:55:30 +0000

    GFM-37/chore: Run lmtp service function in main.go

--- src/goformail/cmd/goformail/main.go
+++ src/goformail/cmd/goformail/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal/interfaces"
+	mailforwarding "gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal/mailForwarding"
 )
 
 func main() {
+	go mailforwarding.LMTPService()
 	interfaces.ServeHttp()
 }
