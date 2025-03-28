10d93f2a934dec02de1c6fa42dbb43f0431d3fc8
sdk194 <seandags750@gmail.com>
2025-03-28 15:53:51 +0000

    GFM-37/test: Add test for loading configs

--- src/goformail/internal/mailForwarding/mailforwarding_test.go
+++ src/goformail/internal/mailForwarding/mailforwarding_test.go
@@ -1,6 +1,7 @@
 package mailforwarding
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"regexp"
 	"testing"
@@ -16,5 +17,20 @@ func TestGetCurrentTime(t *testing.T) {
 	}
 	if !matched {
 		t.Errorf("Not the expected format of [YYYY-MM-DD HH:mm:ss], got %s", formattedTime)
+	}
+}
+
+func TestLoadConfigs(t *testing.T) {
+	configs, err := godotenv.Read("configs.cf")
+	if err != nil {
+		t.Error("Cannot locate config file")
+	}
+
+	//update this test if config names get modified/added
+	configList := []string{"EMAIL_DOMAIN", "DEBUG_MODE", "POSTFIX_ADDRESS", "POSTFIX_PORT", "LMTP_PORT", "TIMEOUT_DURATION", "BUFFER_SIZE"}
+	for _, configParam := range configList {
+		if _, exists := configs[configParam]; !exists {
+			t.Errorf("Config %s is not a parameter within the configs", configParam)
+		}
 	}
 }
