d0c3e5fda9dc2525cbee40311e697df55f909c86
sdk194 <seandags750@gmail.com>
2025-03-30 23:16:22 +0100

    GFM-37/chore: rename forwarding package directory to "forwarding"

--- src/goformail/cmd/goformail/main.go
+++ src/goformail/cmd/goformail/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	config "gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal"
+	"gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal/forwarding"
 	"gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal/interfaces"
-	forwarding "gitlab.computing.dcu.ie/fonseca3/2025-csc1097-fonseca3-dagohos2/internal/mailForwarding"
 )
 
 func main() {
