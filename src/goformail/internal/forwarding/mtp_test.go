96b0438b4151ac108d8384e61fdcd95d5a3a1daa
sdk194 <seandags750@gmail.com>
2025-03-31 23:56:21 +0100

    GFM-37/test: Add negative case for mail receiving

--- src/goformail/internal/forwarding/mtp_test.go
+++ src/goformail/internal/forwarding/mtp_test.go
@@ -255,7 +255,7 @@ func TestSendFailGoodBye(t *testing.T) {
 	}
 }
 
-func TestMailReceiver(t *testing.T) {
+func TestSuccessfulMailReceiver(t *testing.T) {
 	tcpSocket, err := createLMTPSocket("8024")
 	if err != nil {
 		log.Fatal(err)
@@ -339,7 +339,95 @@ func TestMailReceiver(t *testing.T) {
 	} else if data.data != "hello\n.\nmultiple full stops\n.\n" {
 		t.Errorf("The email data was received incorrectly\nExpected:\n hello\n.\nmultiple full stops\n.\n Received: %s\n", data.data)
 	}
+}
 
+func TestFailedMailReceiver(t *testing.T) {
+	tcpSocket, err := createLMTPSocket("8024")
+	if err != nil {
+		log.Fatal(err)
+	}
+	configs := config.LoadConfigs("../../configs.cf")
+	waitGroup := new(sync.WaitGroup)
+
+	defer func(tcpSocket net.Listener) {
+		err = tcpSocket.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		waitGroup.Wait()
+	}(tcpSocket)
+
+	// MOCK MTA
+	go func() {
+		var conn net.Conn
+		conn, err = net.Dial("tcp", "127.0.0.1:8024")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer func(conn net.Conn) {
+			err = conn.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+			t.Log("Goroutine finished within TestMailReceiver")
+			waitGroup.Done()
+		}(conn)
+
+		for {
+			buffer := make([]byte, 4096)
+			var size int
+
+			if err = conn.SetReadDeadline(time.Now().Add(5 * time.Second)); err != nil {
+				log.Fatal(err)
+			}
+
+			size, err = conn.Read(buffer)
+			var netErr net.Error
+			if errors.As(err, &netErr) && netErr.Timeout() {
+				t.Error("Connection timed out, MailReceiver cannot seem to send messages through the " +
+					"connection or the expected responses do not match up")
+				return
+			}
+
+			messages := strings.Lines(string(buffer[:size]))
+			for message := range messages {
+				t.Log(message)
+				switch {
+				case strings.TrimSpace(message) == "220 LMTP Server Ready":
+					if _, err = conn.Write([]byte("Invalid response")); err != nil {
+						log.Fatal(err)
+					}
+				case strings.TrimSpace(message) == "500 Error: command not recognised":
+					return // MailReceiver should return with an error
+				case strings.TrimSpace(message) == "250 SIZE":
+					if _, err = conn.Write([]byte("MAIL FROM:<testing@example.domain>\nRCPT TO:<recipient@example.domain>\nDATA\n")); err != nil {
+						log.Fatal(err)
+					}
+				case strings.TrimSpace(message) == "354 Start mail input; end with <CRLF>.<CRLF>":
+					if _, err = conn.Write([]byte("hello\n.\nmultiple full stops\n.\nQUIT\n")); err != nil {
+						log.Fatal(err)
+					}
+					return
+				}
+			}
+
+		}
+	}()
+	waitGroup.Add(1)
+
+	conn, err := tcpSocket.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, mailError := mailReceiver(conn, 4096, configs)
+
+	waitGroup.Wait()
+
+	if mailError == nil {
+		t.Error("An error type was not returned")
+	}
 }
 
 func TestMailSender(t *testing.T) {
