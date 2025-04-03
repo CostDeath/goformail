fccf013e6b8e46f7ef2f66ee634a0c3c43ee6dec
sdk194 <seandags750@gmail.com>
2025-04-03 16:56:28 +0100

    GFM-44/chore: Remove commented out code

--- src/web/__tests__/pages/loginPage.test.tsx
+++ src/web/__tests__/pages/loginPage.test.tsx
@@ -7,12 +7,4 @@ test("Whole page is rendered", () => {
     expect(screen.getByRole("textbox", {name: "Email"})).toBeDefined();
     expect(screen.getByLabelText("Password")).toBeDefined();
     expect(screen.getByRole("button", {name: "Sign up!"})).toBeDefined();
-
-    /*
-    fireEvent.click(screen.getByRole("button", { name: "Sign up!" }));
-    expect(screen.getByRole("textbox", {name: "Email"})).toBeDefined();
-    expect(screen.getByRole("textbox", {name: "Student ID"})).toBeDefined();
-    expect(screen.getByLabelText("Password")).toBeDefined();
-
-     */
 })
