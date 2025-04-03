b0e0cd764fd73f5be2cb69165f5072292546883a
sdk194 <seandags750@gmail.com>
2025-04-03 12:05:53 +0100

    GFM-44/feat: Use <Link> component for navbar to navigate to different pages

--- src/web/states/linkStateHandler.tsx
+++ src/web/states/linkStateHandler.tsx
@@ -1,22 +1,13 @@
-import {create} from "zustand"
-import {ReactNode} from "react";
-import LoginSignupPage from "@/app/pages/LoginSignupPage";
-
-interface LinkState {
-    name: string;
-    component: ReactNode;
-    changePage: (page: ReactNode, pageName: string) => void;
-}
-
 //enums for names associated with each
 export const PageName = {
-    LOGINSIGNUP: "Login/Signup",
+    LOGIN: "Login/Signup",
     MAILINGLISTS: "Mailing Lists",
     APPROVALREQUESTS: "Email Approval Requests",
 }
 
-export const switchPage = create<LinkState>()((set) => ({
-    name: PageName.LOGINSIGNUP,
-    component: <LoginSignupPage />,
-    changePage: (page: ReactNode, pageName: string) => set((state) => ({component: page, name: pageName})),
-}))
+export const LinkTo = {
+    LOGIN: "/",
+    SIGNUP: "/signup",
+    MAILINGLISTS: "/mailingLists",
+    APPROVALREQUESTS: "/approvals"
+}
