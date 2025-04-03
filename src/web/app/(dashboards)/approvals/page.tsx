1be9b65fbd5c5c2b7a13cdf89176ffdb8758ec91
sdk194 <seandags750@gmail.com>
2025-04-03 11:41:12 +0100

    GFM-44/feat: Make separate page for email approvals

---
+++ src/web/app/(dashboards)/approvals/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import ApprovalRequestsTable from "@/components/emailApprovalRequests/approvalRequestsTable";
+import Pagination from "@/components/pagination";
+import Modal from "@/components/modal";
+import EmailView from "@/components/emailApprovalRequests/emailView";
+import {useModal} from "@/states/modalStateHandler";
+import {Suspense} from "react";
+
+export default function Page() {
+    const showModal = useModal((state) => state.toggled)
+    return (
+        <div className="w-full">
+            <ApprovalRequestsTable api="placeholder" />
+            <Suspense>
+                <Pagination totalPages={1} />
+            </Suspense>
+
+            {showModal && (
+                <Modal><EmailView /></Modal>
+            )}
+        </div>
+    )
+}
