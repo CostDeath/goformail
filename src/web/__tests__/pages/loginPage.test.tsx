bd2c06a5a19d5b7e9eb5e7338e8026af40e14959
sdk194 <seandags750@gmail.com>
2025-04-12 17:41:25 +0100

    GFM-52/test: Fix up tests for login form as it expected a button html tag which should not have been the tag used

--- src/web/__tests__/pages/loginPage.test.tsx
+++ src/web/__tests__/pages/loginPage.test.tsx
@@ -6,5 +6,5 @@ test("Whole page is rendered", () => {
     render(<Page />);
     expect(screen.getByRole("textbox", {name: "Email"})).toBeDefined();
     expect(screen.getByLabelText("Password")).toBeDefined();
-    expect(screen.getByRole("button", {name: "Sign up!"})).toBeDefined();
+    expect(screen.getByTestId("to-sign-up")).toBeDefined();
 })
--- src/web/app/page.tsx
+++ src/web/app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
                 <Card><LoginForm/></Card>
                 <div className="grid grid-cols-2 py-5 px-5">
                     <p>Don&apos;t have an account?</p>
-                    <Link href={LinkTo.SIGNUP} className="text-blue-400 font-bold px-7">
+                    <Link href={LinkTo.SIGNUP} className="text-blue-400 font-bold px-7" data-testid="to-sign-up">
                         Sign up!
                     </Link>
                 </div>
