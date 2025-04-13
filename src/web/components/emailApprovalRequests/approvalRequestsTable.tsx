9716d03a734a8b0bd4dccddc61e1d07f9bcf1b4d
sdk194 <seandags750@gmail.com>
2025-04-13 23:35:08 +0100

    GFM-52/chore: Modify email approval page ui to match dark colours

--- src/web/components/emailApprovalRequests/approvalRequestsTable.tsx
+++ src/web/components/emailApprovalRequests/approvalRequestsTable.tsx
@@ -26,9 +26,9 @@ export default function ApprovalRequestsTable({currentPage, api}: {
 
 
     return (
-        <div className="min-w-full table text-gray-900 border-gray-400 border-[1px]">
+        <div className="min-w-full table text-gray-900 shadow-[0_3px_10px_-1px_rgba(0,0,0,1)]">
             <div className="table-header-group text-left text-sm font-normal">
-                <div data-testid="table-head" className="table-row">
+                <div data-testid="table-head" className="table-row bg-neutral-800/45 text-neutral-300">
                     <div className="table-cell border-b border-black text-left px-4 py-5 font-bold sm:pl-6">
                         Mailing List Email {/*data.fact*/}
                     </div>
@@ -41,36 +41,36 @@ export default function ApprovalRequestsTable({currentPage, api}: {
                 </div>
             </div>
             <div data-testid="table-body" className="table-row-group">
-                <div className="table-row shadow-inner hover:bg-gray-100 hover:cursor-pointer" onClick={() => toggleModal(true)}>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                <div className="table-row shadow-inner text-neutral-300 hover:bg-neutral-600/75  hover:cursor-pointer" onClick={() => toggleModal(true)}>
+                    <div className="table-cell border-black border-b py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             exampleentry@email.com
                         </div>
                     </div>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                    <div className="table-cell border-black border-b py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             Testing Forwarding
                         </div>
                     </div>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                    <div className="table-cell border-black border-b py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             09/01/2025
                         </div>
                     </div>
                 </div>
 
-                <div className="table-row shadow-inner hover:bg-gray-100 hover:cursor-pointer" onClick={() => toggleModal(true)}>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                <div className="table-row shadow-inner text-neutral-300 hover:bg-neutral-600/75  hover:cursor-pointer" onClick={() => toggleModal(true)}>
+                    <div className="table-cell black border-black py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             exampleentry2@email.com
                         </div>
                     </div>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                    <div className="table-cell black border-black py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             Testing Testing...
                         </div>
                     </div>
-                    <div className="table-cell border-gray-200 border-b py-3 text-sm">
+                    <div className="table-cell border-black border-b py-3 text-sm">
                         <div className="whitespace-nowrap py-3 pl-6 pr-3 flex items-center gap-3">
                             09/01/2025
                         </div>
