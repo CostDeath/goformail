4dab56da3ade9077d704070e43c19238845ce755
sdk194 <seandags750@gmail.com>
2025-04-18 20:31:36 +0100

    GFM-52/fix: Wrap mailing list email list table to a component to handle grabbing the query parameter

--- src/web/app/(dashboards)/mailingLists/list/page.tsx
+++ src/web/app/(dashboards)/mailingLists/list/page.tsx
@@ -1,34 +1,17 @@
 "use client"
 
-import {useSearchParams} from "next/navigation";
-import useSWR from "swr";
-import ListEmailsTable from "@/components/list/listEmailsTable";
 import Pagination from "@/components/pagination";
 import {Suspense} from "react";
+import ListEmailsTableWrapper from "@/components/list/listEmailsTableWrapper";
 
 export default function Page() {
-    const searchParams = useSearchParams()
-    const id = searchParams.get("id")
 
-    const fetcher = (...args: Parameters<typeof fetch>) =>
-        fetch(...args).then((res) => res.json())
-
-    const {data, error} = useSWR(`https://jsonplaceholder.typicode.com/posts/${id}`, fetcher)
-
-    if (error) return <div>Error</div>
-    if (!data) {
-        return <div>Loading...</div>
-    } else if (!data.id) {
-        return <div>Error</div>
-    }
-
-
-    console.log(data)
 
     return (
         <div className="w-full">
-            <h1 className="font-bold py-5 px-2 text-2xl">{data.title}</h1>
-            <ListEmailsTable/>
+            <Suspense>
+                <ListEmailsTableWrapper />
+            </Suspense>
 
             <Suspense>
                 <Pagination totalPages={1} />
