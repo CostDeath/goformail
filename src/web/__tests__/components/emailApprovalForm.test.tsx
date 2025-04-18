1e7d11a912776d068a6a7ed4875b3bc700beb2d2
sdk194 <seandags750@gmail.com>
2025-04-18 18:06:47 +0100

    GFM-52/test: Update email view test and make test for email approval form component

--- src/web/__tests__/components/emailApprovalForm.test.tsx
+++ src/web/__tests__/components/emailApprovalForm.test.tsx
@@ -1,13 +1,15 @@
+
 import {expect, test} from "vitest";
 import {render, screen} from "@testing-library/react";
-import EmailView from "@/components/emailView";
+import EmailApprovalForm from "@/components/emailApprovalRequests/emailApprovalForm";
 
 test("Email view is rendered", () => {
-    render(<EmailView id={"1"} />);
+    render(<EmailApprovalForm id={"1"} />);
 
     expect(screen.getByTestId("soc-email")).toBeDefined();
     expect(screen.getByTestId("email-title")).toBeDefined();
     expect(screen.getByTestId("email-subject")).toBeDefined();
     expect(screen.getByTestId("email-content")).toBeDefined();
-
+    expect(screen.getByRole("button", {name: "Approve"})).toBeDefined();
+    expect(screen.getByRole("button", {name: "Reject"})).toBeDefined();
 })
