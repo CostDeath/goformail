f384213d91ba9f0a57a7cf5d479b17d8bb7cb3d3
sdk194 <seandags750@gmail.com>
2025-04-20 14:51:06 +0100

    GFM-52/feat: Add card to create and edit mailing list pages

--- src/web/app/(dashboards)/mailingLists/create/page.tsx
+++ src/web/app/(dashboards)/mailingLists/create/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import ListCreationForm from "@/components/createList/listCreationForm";
+import Card from "@/components/card";
 
 
 export default function Page() {
     return (
         <>
-            <h1 className="font-bold px-2 text-2xl">Create a new mailing list</h1>
+            <Card>
+            <h1 className="font-bold px-2 py-5 text-2xl">Create a new mailing list</h1>
+                <hr/>
             <ListCreationForm />
+            </Card>
         </>
     )
 }
--- src/web/app/(dashboards)/mailingLists/list/edit/page.tsx
+++ src/web/app/(dashboards)/mailingLists/list/edit/page.tsx
@@ -2,14 +2,17 @@
 
 import ListEditForm from "@/components/editList/listEditForm";
 import {Suspense} from "react";
+import Card from "@/components/card";
 
 
 export default function Page() {
     return (
         <>
+            <Card>
             <Suspense>
                 <ListEditForm />
             </Suspense>
+            </Card>
         </>
     )
 }
