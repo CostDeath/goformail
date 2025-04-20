4ed23c47f116dc9635ec9400b8858107fea72c0d
sdk194 <seandags750@gmail.com>
2025-04-20 22:24:34 +0100

    GFM-52/test: Create user table component test

---
+++ src/web/__tests__/components/userTable.test.tsx
@@ -0,0 +1,10 @@
+import {expect, test} from "vitest";
+import {render, screen} from "@testing-library/react";
+import UserTable from "@/components/management/userTable";
+
+test("mailing list table is rendered", () => {
+    render(<UserTable />);
+
+    expect(screen.getByTestId("table-head")).toBeDefined();
+    expect(screen.getByTestId("table-body")).toBeDefined();
+})
