73fa2c6a4f07d9cb3f363d9988372c285ddc3434
sdk194 <seandags750@gmail.com>
2025-04-20 15:58:03 +0100

    GFM-52/feat: Create user page

---
+++ src/web/components/permissions.tsx
@@ -0,0 +1,32 @@
+
+
+export const permissionsList = [
+    {
+        label: "View Lists",
+        value: false,
+    },
+    {
+        label: "View Emails",
+        value: false,
+    },
+    {
+        label: "Approve Emails",
+        value: false,
+    },
+    {
+        label: "Manage Mailing Lists",
+        value: false,
+    },
+    {
+        label: "Manage Own Account",
+        value: false,
+    },
+    {
+        label: "Manage Other Accounts",
+        value: false
+    },
+    {
+        label: "Change Accounts' permissions",
+        value: false
+    }
+]
