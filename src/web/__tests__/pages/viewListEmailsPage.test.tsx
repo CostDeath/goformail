d904b54214eccd3bb6bc4dcea5e9ee08beba9a70
sdk194 <seandags750@gmail.com>
2025-04-21 11:40:49 +0100

    GFM-51/test: List email page test

---
+++ src/web/__tests__/pages/viewListEmailsPage.test.tsx
@@ -0,0 +1,60 @@
+import {expect, test, vitest} from "vitest";
+import {render, screen} from "@testing-library/react";
+import useSWR from "swr";
+import Page from "@/app/(dashboards)/mailingLists/list/page";
+
+vitest.mock("next/navigation", () => {
+    const actual = vitest.importActual("next/navigation");
+    return {
+        ...actual,
+        useSearchParams: vitest.fn(() => ({
+            get: (key: string) => {
+                if (key === "id") return "1"
+                return null
+            }
+        })),
+    }
+})
+
+
+test("List Email page is loading", async () => {
+    useSWR.mockReturnValue({
+        data: undefined
+    })
+    const wrapper = render(<Page/>);
+
+    expect(wrapper.getByText("Loading...")).toBeDefined();
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+
+    wrapper.unmount()
+})
+
+vitest.mock("swr")
+
+test("List Email Table page has loaded and is rendered", async () => {
+    useSWR.mockReturnValue({
+        data: {id: "1"}
+    })
+    const wrapper = render(<Page />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+
+    expect(screen.getByTestId("table-head")).toBeDefined();
+    expect(screen.getByTestId("table-body")).toBeDefined();
+    wrapper.unmount()
+})
+
+test("List Email Table page has loaded but given data was invalid", async () => {
+    useSWR.mockReturnValue({
+        data: {}
+    })
+    const wrapper = render(<Page />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+    expect(wrapper.getByText("Error")).toBeDefined();
+    wrapper.unmount()
+})
