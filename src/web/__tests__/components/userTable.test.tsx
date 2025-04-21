107083ea02739244527b11d77fe11e1aa0ed8a39
sdk194 <seandags750@gmail.com>
2025-04-21 11:23:08 +0100

    GFM-52/fix: User table test description typo

--- src/web/__tests__/components/userTable.test.tsx
+++ src/web/__tests__/components/userTable.test.tsx
@@ -2,7 +2,7 @@ import {expect, test} from "vitest";
 import {render, screen} from "@testing-library/react";
 import UserTable from "@/components/management/userTable";
 
-test("mailing list table is rendered", () => {
+test("User table is rendered", () => {
     render(<UserTable />);
 
     expect(screen.getByTestId("table-head")).toBeDefined();
