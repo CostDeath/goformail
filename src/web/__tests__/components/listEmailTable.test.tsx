5d58887c8ce9ffd3321bff1cb9812c68f82ccc0f
sdk194 <seandags750@gmail.com>
2025-04-21 02:03:20 +0100

    GFM-52/test: Create List Email Table test

---
+++ src/web/__tests__/components/listEmailTable.test.tsx
@@ -0,0 +1,63 @@
+import {expect, test, vitest} from "vitest";
+import {fireEvent, render, screen} from "@testing-library/react";
+import EditUserForm from "@/components/editUser/editUserForm";
+import useSWR from "swr";
+import {EmailChecker} from "@/__tests__/util/formCheckers";
+import ListEditForm from "@/components/editList/listEditForm";
+import ListEmailsTable from "@/components/list/listEmailsTable";
+
+vitest.mock("next/navigation", () => {
+    const actual = vitest.importActual("next/navigation");
+    return {
+        ...actual,
+        useSearchParams: vitest.fn(() => ({
+            get: (key: string) => {
+                if (key === "id") return "1"
+                return null
+            }
+        })),
+    }
+})
+
+
+test("List Email Table is loading", async () => {
+    useSWR.mockReturnValue({
+        data: undefined
+    })
+    const wrapper = render(<ListEmailsTable/>);
+
+    expect(wrapper.getByText("Loading...")).toBeDefined();
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+
+    wrapper.unmount()
+})
+
+vitest.mock("swr")
+
+test("List Email Table component has loaded and is rendered", async () => {
+    useSWR.mockReturnValue({
+        data: {id: "1"}
+    })
+    const wrapper = render(<ListEmailsTable />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+
+    expect(screen.getByTestId("table-head")).toBeDefined();
+    expect(screen.getByTestId("table-body")).toBeDefined();
+    wrapper.unmount()
+})
+
+test("List Email Table component has loaded but given data was invalid", async () => {
+    useSWR.mockReturnValue({
+        data: {}
+    })
+    const wrapper = render(<EditUserForm />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+    expect(wrapper.getByText("Error")).toBeDefined();
+    wrapper.unmount()
+})
