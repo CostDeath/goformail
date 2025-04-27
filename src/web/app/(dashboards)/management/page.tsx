0bcdcb9e0777e4f93ed3467d79cafb752629d01a
sdk194 <seandags750@gmail.com>
2025-04-27 11:12:20 +0100

    GFM-51/feat: logs page

--- src/web/app/(dashboards)/management/page.tsx
+++ src/web/app/(dashboards)/management/page.tsx
@@ -6,7 +6,12 @@ export default function Page() {
     return (
         <>
             <div className="py-5">
-                <Link href="/management/add.html" className="bg-cyan-600 text-white hover:bg-cyan-500 px-3 py-2 rounded-md font-bold">+ New User</Link>
+                <span>
+                    <Link href="/management/add.html" className="bg-cyan-600 text-white hover:bg-cyan-500 px-3 py-2 rounded-md font-bold">+ New User</Link>
+                </span>
+                <span className="px-5">
+                    <Link href="/management/logs" className="bg-cyan-600 text-white hover:bg-cyan-500 px-3 py-2 rounded-md font-bold">Audit Logs</Link>
+                </span>
             </div>
             <UserTable />
         </>
