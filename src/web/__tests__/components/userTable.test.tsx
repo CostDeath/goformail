a7103a16650043a2f992fa3e61b63b76d9a2cebc
sdk194 <seandags750@gmail.com>
2025-04-29 18:54:22 +0100

    GFM-64/test: Update user table component test

--- src/web/__tests__/components/userTable.test.tsx
+++ src/web/__tests__/components/userTable.test.tsx
@@ -1,10 +1,61 @@
-import {expect, test} from "vitest";
-import {render, screen} from "@testing-library/react";
+import {expect, test, vitest} from "vitest";
+import {render} from "@testing-library/react";
 import UserTable from "@/components/management/userTable";
+import useSWR from "swr";
 
-test("User table is rendered", () => {
-    render(<UserTable />);
+vitest.mock("swr")
 
-    expect(screen.getByTestId("table-head")).toBeDefined();
-    expect(screen.getByTestId("table-body")).toBeDefined();
+test("User table is loading", async () => {
+    useSWR.mockReturnValue({
+        data: undefined
+    })
+    const wrapper = render(<UserTable />);
+
+    expect(wrapper.getByText("Loading")).toBeDefined();
+    wrapper.unmount()
+
+})
+
+test("User table has loaded", async () => {
+    useSWR.mockReturnValue({
+        data: {message: "Successfully fetched users!", data: [{id: 1, email: "test@user.com"}]}
+    })
+
+    const wrapper = render(<UserTable />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+    expect(wrapper.getByTestId("table-head")).toBeDefined();
+    expect(wrapper.getByTestId("table-body")).toBeDefined();
+    expect(wrapper.getByRole("link", {name: "test@user.com"})).toBeDefined()
+    wrapper.unmount()
+})
+
+test("User table has loaded but given data was invalid", async () => {
+    useSWR.mockReturnValue({
+        data: {message: "Error"}
+    })
+
+    const wrapper = render(<UserTable />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+
+    expect(wrapper.getByText("Error")).toBeDefined();
+    wrapper.unmount()
+})
+
+test("User table has loaded but there is no data to show", async () => {
+    useSWR.mockReturnValue({
+        data: {message: "Successfully fetched users!"}
+    })
+
+    const wrapper = render(<UserTable />)
+
+    vitest.useFakeTimers()
+    vitest.runAllTimers()
+    expect(wrapper.getByTestId("table-head")).toBeDefined();
+    expect(wrapper.getByTestId("table-body")).toBeDefined();
+    expect(wrapper.getByText("No Data to Show")).toBeDefined();
+    wrapper.unmount()
 })
