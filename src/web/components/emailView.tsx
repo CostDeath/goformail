e24ea8534e3c6c6d81780e32ae345a24bf8b969d
sdk194 <seandags750@gmail.com>
2025-05-02 11:43:06 +0100

    GFM-70/test: Update test for viewing singular email

--- src/web/components/emailView.tsx
+++ src/web/components/emailView.tsx
@@ -50,7 +50,6 @@ export default function EmailView() {
     return (
         <>
             <div className="">
-                <div data-testid="soc-email" className="text-xl py-3"> {/* Title of email goes here */}</div>
                 <div data-testid="email-title" className="border-b border-black py-2">To: {rcpts}</div>
                 <div data-testid="email-subject" className="border-b border-black py-2">From: {email.sender}</div>
             </div>
