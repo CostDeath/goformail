86f9c6f39309d594d93fcc4efdd6732fe7442d3e
sdk194 <seandags750@gmail.com>
2025-05-02 10:56:08 +0100

    GFM-70/feat: Add email view

--- src/web/app/(dashboards)/mailingLists/list/email/page.tsx
+++ src/web/app/(dashboards)/mailingLists/list/email/page.tsx
@@ -1,9 +1,12 @@
 import EmailView from "@/components/emailView";
+import {Suspense} from "react";
 
 export default function Page() {
     return (
         <>
-            <EmailView />
+            <Suspense>
+                <EmailView />
+            </Suspense>
         </>
     )
 }
